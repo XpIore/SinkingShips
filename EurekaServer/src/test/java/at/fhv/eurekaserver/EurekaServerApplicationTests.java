2e4fa47e88accb27e30937a44ed4adb71743dde1
Felix <felix.theissl@students.fhv.at>
2025-03-24 19:12:45 +0100

    commit for safety (does not compile)

---
+++ EurekaServer/src/test/java/at/fhv/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
